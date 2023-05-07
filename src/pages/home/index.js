a531fa119d04d2305e00c0718715910d5ea0db67
Thayrone-L <thayroneliaodasilva@gmail.com>
2023-05-06 22:16:52 -0300

    alterações no projeto

---
+++ src/pages/home/index.js
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <div>
+    <h1>Filmes</h1>
+<ul>
+  <li>
+<img src="https://cdn.marvel.com/content/1x/prequel_comic_cover_larger_size.jpg" alt="Miranha"/>
+<span>Spider Man</span>
+  </li>
+  <li>
+  <img src="https://cdn.marvel.com/content/1x/prequel_comic_cover_larger_size.jpg" alt="Miranha"/>
+<span>Spider Man</span>
+  </li>
+  <li>
+  <img src="https://cdn.marvel.com/content/1x/prequel_comic_cover_larger_size.jpg" alt="Miranha"/>
+<span>Spider Man</span>
+  </li>
+</ul>
+
+    </div>
+  )
+}
+
+export default Home;
