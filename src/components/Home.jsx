64bb8c7e480d53b0012592d1c9073b6d8c629e5c
Thayrone-L <thayroneliaodasilva@gmail.com>
2023-05-06 21:21:23 -0300

    Alterações na Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+      return (
+        <div>
+          <div className="row">
+            <h1>Aqui é a home</h1>
+            </div>
+        </div>
+      );
+    }
+  
+  }
+  
+  export default Home;
